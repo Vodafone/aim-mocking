6c28646212f418a339382de8f2022f0b617edf8b
Alex G <alessandro.giordo@vodafone.com>
2023-08-03 12:17:41 +0100

    feat: changed naming last run

--- packages/logger/src/index.types.ts
+++ packages/logger/src/index.types.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
-// declare module '@vodafoneuk/util-aim-logger' {
 
 interface Group {
   yarn: Yarn
